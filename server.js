5dfe7506457161e53a0f022f9c12e1c44e3c13bf
masonkarsevar <masonkarsevar@gmail.com>
2019-09-10 12:33:20 -0700

    carRoute get request returns the seed values

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const carRoute = require('./routes/carRoute.js');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/cars', carRoute);
 
 server.use('/api', (req, res) => {
     res.status(200).json({message: 'testing testing welcome to the base api route'})
